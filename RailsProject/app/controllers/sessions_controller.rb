734ed37e26f1838192d3eb1c4e52f02f9b49d59f
Dennis Chung <chungdennis6@gmail.com>
2019-09-17 06:47:08 +0000

    created create_users.rb

--- RailsProject/app/controllers/sessions_controller.rb
+++ RailsProject/app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+    @user = user.new
+  end
+
+  def create
+    @user = User.find(params[:user_name])
+    if @user && @user.authenticate(params[:password])
+      sessions[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to signin_path
+    end
+
+    def destroy
+      session.clear
+      redirect_to '/'
+  end
+
 end
