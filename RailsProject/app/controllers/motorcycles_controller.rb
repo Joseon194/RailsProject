809a9909f386670be6d09032ee3e5657fb8903e5
Dennis Chung <chungdennis6@gmail.com>
2019-09-19 22:57:10 +0000

    added lines to motorcycles.rb

--- RailsProject/app/controllers/motorcycles_controller.rb
+++ RailsProject/app/controllers/motorcycles_controller.rb
@@ -1,2 +1,39 @@
 class MotorcyclesController < ApplicationController
+
+  def new
+    @motorcycle = Motorcycle.new
+  end
+
+  def create
+    @motorcycle = Motorcycle.new(car_params)
+    if @motorcycle.save
+      redirect_to @motorcycle
+    else
+      redirect_to new_motorcycle_path
+    end
+  end
+
+  def edit
+    @motorcycle = Motorcycle.find_by(id: params[:id])
+  end
+
+  def update
+    @motorcycle= Motorcycle.find_by(id: params[:id])
+    @motorcycle.update(motorcycle_params)
+    redirect_to motorcycle_path(@motorcycle)
+  end
+
+  def show
+    @motorcycle = Motorcycle.find_by(id: param[:id])
+  end
+
+  def index
+    @motorcycle = Motorcycle.all
+  end
+
+  private
+
+  def motorcycle_params
+    params_require(:motorcycle).permit(:name, :horsepower, :top_speed, :handling_rating, :comfort_rating)
+  end
 end
